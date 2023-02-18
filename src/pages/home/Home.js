f2c2524eefad28e91d38ac6aa3535133aa691c62
abdelhak-org <abdelhak.org@gmail.com>
2023-02-18 22:08:12 +0100

    add a section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Container from '../../layout/Container'
+  import React from 'react'
+  import Container from '../../layout/Container';
+  import useFetchData from '../../hooks/useFetchData'
+  const Home = () => {
+    const  data = useFetchData("https://jsonplaceholder.typicode.com/posts")
+    return (
+      <Container className ="min-h-screen bg-yellow-200 pr-20 text-center font-2xl
+      flex justify-between items-start flex-wrap
+      " >
+        {
+          data.slice(0,8).map((item)=>
+          <div key={item.id} className='w-[22%] border  border-blue-300 p-2 m-2 rounded-sm'>
+            <h3 className='p-2 text-gray-800 font-semibold  ' >{item.title}</h3>
+            <p className="p-2 mx-auto my-2  ">{item.body}</p>
+          </div>
+          )
+        }
+      </Container>
+    )
+  }
 
-const Home = () => {
-  return (
-    <Container className ="h-screen bg-yellow-200 text-center font-2xl" >
-      Home Page
-    </Container>
-  )
-}
-
-export default Home
+  export default Home
